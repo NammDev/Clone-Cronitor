08ccea707a7fcca367129a0c4f04425fac4b0b66
Nam <namdeveloper.ca@gmail.com>
2022-11-25 19:10:50 +0700

    Done Header & Trusted Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
 const productBtn = document.querySelector('.navbar__link:first-child')
+const iconBtn = document.querySelector('.navbar__ham')
 const productTray = document.querySelector('.product-tray')
+const productTrayMb = document.querySelector('.nav-mobile')
+const dropdownMb = document.querySelector('.nav-mobile__dropdown')
+const productItemMb = document.querySelector('.nav-mobile__product-tray')
 
 productBtn.addEventListener('click', () => {
   productTray.classList.toggle('open')
+})
+
+iconBtn.addEventListener('click', () => {
+  iconBtn.classList.toggle('open')
+  productTrayMb.classList.toggle('open')
+})
+
+dropdownMb.addEventListener('click', () => {
+  dropdownMb.classList.toggle('open')
+  productItemMb.classList.toggle('close')
 })
